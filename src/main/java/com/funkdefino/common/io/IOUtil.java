675a9c8f68f194fa55c707f602fb29534a7a9dcd
David Lang <david@proxsys.co.uk>
2018-01-16 13:30:03 +0000

    Added loadResource() overload

--- src/main/java/com/funkdefino/common/io/IOUtil.java
+++ src/main/java/com/funkdefino/common/io/IOUtil.java
@@ -1,5 +1,6 @@
 package com.funkdefino.common.io;
 
+import com.funkdefino.common.util.UtilException;
 import com.funkdefino.common.util.xml.XmlDocument;
 import com.funkdefino.common.util.xml.XmlException;
 
@@ -52,5 +53,50 @@ public final class IOUtil {
     return doc;
 
   } // loadResource()
+
+
+  /**
+   * This reads a sequence of bytes from resources.
+   * @param clz the calling class.
+   * @param resource the resource.
+   * @throws IOException on error.
+   * @throws UtilException on error.
+   * @return the array.
+   */
+    public static byte[] loadResource(Class clz, String resource)
+                         throws IOException, UtilException {
+
+        ByteArrayOutputStream baos = null;
+        InputStream is = null;
+        byte[] arr;
+
+        try {
+
+            ClassLoader classLoader = clz.getClassLoader();
+            if((is = classLoader.getResourceAsStream(resource)) == null) {
+                String sExcp = String.format("Unable to load resource file '%s'", resource);
+                throw new UtilException(sExcp);
+            }
+
+            baos = new ByteArrayOutputStream();
+            byte[] data = new byte[256];
+            int read;
+
+            while((read = is.read(data, 0, data.length)) != -1) {
+              baos.write(data, 0, read);
+            }
+
+            baos.flush();
+            arr = baos.toByteArray();
+
+        }
+        finally {
+            close(baos);
+            close(is);
+        }
+
+        return arr;
+
+    } // loadResource()
 
 } // class IOUtil
